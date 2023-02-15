62c3b455ecec8ec6d81fae4ce956f92b05a8a420
unknown <null>
2023-02-15 01:22:08 -0500

    Configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <link rel="shortcut icon" href="{{ asset('img/logo_woob.JPG') }}" />
+
+        <title>{{ env('APP_NAME') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
